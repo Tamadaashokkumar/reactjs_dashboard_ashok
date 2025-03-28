c26d85698b8091d1ca1982eebab3400d4140d5ed
Tamadaashokkumar <tamadaashokkumar4@gmail.com>
2025-03-28 10:46:05 +0530

    add url

--- src/vendorDashboard/api.js
+++ src/vendorDashboard/api.js
@@ -1,6 +1,3 @@
 //export const url = "http://localhost:4000"
 
-//export const url = "https://nodejs-backend-ashok.onrender.com"
-//export const url = "https://nodejs-backend-ashok-cppf.vercel.app"
-
-export const url = "https://backend-new-ashok.onrender.com"
+const url = "https://backend-ashok.onrender.com"
